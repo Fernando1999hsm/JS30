49cd728245c2ddae8d3d3ee3d9af37c0b1dac9b1
Fernando1999hsm <fernandocalva15@gmail.com>
2023-03-21 12:44:35 -0600

    Dia 29, completado

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,58 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const button = document.querySelectorAll('[data-time]');
+
+function timer(seconds){
+    clearInterval(countdown);
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    console.log(now,then);
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+
+    countdown = setInterval(()=>{
+        const secondsLeft = Math.round(( then - Date.now() ) / 1000);
+        //check if the timer went 0
+        if(secondsLeft <= 0){
+            clearInterval(countdown);
+            return;
+        }
+        //display it
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds){
+    console.log(seconds);
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    console.log(minutes,remainderSeconds);
+    const display = `${minutes}:${remainderSeconds < 10 ? '0':''}${remainderSeconds}`;
+    timerDisplay.textContent = display;
+    document.title = display;
+}
+
+function displayEndTime(timestamp){
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be back at ${hour > 12 ? 'AM' : 'PM'}${hour > 12 ? hour-12 : hour}:${minutes < 10 ? '0':''}${minutes}`;
+}
+
+function startTimer(){
+    console.log(this);
+    console.log(this.dataset);
+    const seconds = parseInt(this.dataset.time);
+    console.log(seconds);
+    timer(seconds);
+}
+
+button.forEach(button => button.addEventListener('click',startTimer));
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+});
